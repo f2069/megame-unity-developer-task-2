6a3069095e81583ddaa386444098d17be8164acd
f2069 <fatalerror2069@gmail.com>
2022-07-26 01:10:07 +0500

    add follow cursor;

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 namespace MegameAnimatoins {
     public class Player : MonoBehaviour {
         [SerializeField] private Animator animator;
-        [SerializeField] private Transform activeWeapon;
+        [SerializeField] private LayerMask groundLayers;
+        [SerializeField] private Transform spineNode;
+        [SerializeField] private Transform startWeapon;
+        [SerializeField] private Transform shootingHandPosition;
         [SerializeField] private Transform sword;
         [SerializeField] private float fatalityRadius;
         [SerializeField] private float moveSpeed = 1f;
@@ -18,14 +21,20 @@ namespace MegameAnimatoins {
         private Camera _camera;
 
         private float _turnVelocity;
+        private Rigidbody _rigidbody;
 
         private void Awake() {
             _camera = Camera.main;
             _characterController = GetComponent<CharacterController>();
             _userInput = GetComponent<UserInputHandler>();
+            _rigidbody = GetComponent<Rigidbody>();
 
             _userInput.OnMoveEvent += OnInputMove;
             _userInput.OnRotateEvent += OnInputRotate;
+        }
+
+        private void Start() {
+            InitWeapon();
         }
 
         private void Update() {
@@ -47,16 +56,65 @@ namespace MegameAnimatoins {
             animator.SetFloat(VelocityAnimationKey, direction.normalized.magnitude);
         }
 
+        private void LateUpdate() {
+            var lookTarget = _lookTarget;
+            lookTarget.y = spineNode.position.y;
+
+            // Debug.DrawRay(
+            //     spineNode.position,
+            //     lookTarget - spineNode.position,
+            //     Color.red
+            // );
+
+            var signedAngle = Vector3.SignedAngle(
+                lookTarget - spineNode.position,
+                transform.forward,
+                Vector3.up
+            );
+
+            spineNode.localEulerAngles += Quaternion.Euler(signedAngle, 0, 0).eulerAngles;
+        }
+
         private void OnDestroy() {
             _userInput.OnMoveEvent -= OnInputMove;
             _userInput.OnRotateEvent -= OnInputRotate;
         }
 
+        private Vector3 _lookTarget;
+
         private void OnInputRotate(Vector3 mouseposition) {
+            // var cameraPosition = _camera.transform.position;
+            var maxDistanceRay = 100f;
+            var screenPointRay = _camera.ScreenPointToRay(
+                new Vector3(
+                    mouseposition.x,
+                    mouseposition.y,
+                    maxDistanceRay
+                )
+            );
+
+            if (!Physics.Raycast(screenPointRay, out var pointRaycastHit, maxDistanceRay, groundLayers)) {
+                return;
+            }
+
+            // var targetPosition = pointRaycastHit.point - cameraPosition;
+            // Debug.DrawRay(
+            //     cameraPosition,
+            //     targetPosition,
+            //     Color.yellow
+            // );
+
+            _lookTarget = pointRaycastHit.point;
         }
 
-        private void OnInputMove(Vector3 direction) {
-            _inputDirection = direction.normalized;
+        private void OnInputMove(Vector3 direction)
+            => _inputDirection = direction.normalized;
+
+        private void InitWeapon() {
+            Instantiate(
+                startWeapon.gameObject,
+                shootingHandPosition
+            );
         }
     }
 }
